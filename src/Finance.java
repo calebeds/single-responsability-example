a0956f8bf70cf143f188bc824a446b1cc3839e66
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-04-24 14:39:31 -0300

    add single responsability example

---
+++ src/Finance.java
@@ -0,0 +1,6 @@
+public class Finance {
+    public double calcIncomeTaxForCurrentYear(Employee employee) {
+        //income tax logic using employee passed in
+        return 0.0;
+    }
+}
---
+++ src/HRPromotions.java
@@ -0,0 +1,6 @@
+public class HRPromotions {
+    public boolean isPromotionDueThisYear(Employee employee) {
+        //promotion logic implementation is using the employee information passed in
+        return true;
+    }
+}
